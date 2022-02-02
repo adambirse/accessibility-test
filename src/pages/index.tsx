2cb6c65d235500233af9e0a788f43b308f06aedf
Adam Birse <adambirse@gmail.com>
2022-02-02 19:41:49 +0000

    Resolve Axe accessibility issues.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,32 @@
+import { Helmet } from "react-helmet";
+
 import { Breadcrumb } from "../components/Breadcrumb/Breadcrumb";
 
 export default function Page() {
   return (
     <>
-      <h1>Accessible components</h1>
-      <Breadcrumb
-        links={[
-          {
-            label: "Link 1",
-            href: "",
-          },
-          {
-            label: "Link 2",
-            href: "",
-          },
-          {
-            label: "Link 3",
-            href: "",
-          },
-        ]}
-      />
+      <Helmet htmlAttributes={{ lang: "en" }}>
+        <title>Accessible components</title>
+      </Helmet>
+      <main>
+        <h1>Accessible components</h1>
+        <Breadcrumb
+          links={[
+            {
+              label: "Link 1",
+              href: "",
+            },
+            {
+              label: "Link 2",
+              href: "",
+            },
+            {
+              label: "Link 3",
+              href: "",
+            },
+          ]}
+        />
+      </main>
     </>
   );
 }
