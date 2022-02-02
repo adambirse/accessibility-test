8913c22726f86273dce2053ad79fc314d61392be
Adam Birse <adambirse@gmail.com>
2022-02-02 19:02:01 +0000

    Refactor and add test.

--- src/components/Breadcrumb/Breadcrumb.test.tsx
+++ src/components/Breadcrumb/Breadcrumb.test.tsx
@@ -3,25 +3,28 @@ import React from "react";
 
 import { Breadcrumb } from "./Breadcrumb";
 
+const links = [
+  {
+    label: "Link 1",
+    href: "",
+  },
+  {
+    label: "Link 2",
+    href: "",
+  },
+  {
+    label: "Link 3",
+    href: "",
+  },
+];
+
 describe("Breadcrumb", () => {
   it("should render without throwing an error", function () {
-    mount(
-      <Breadcrumb
-        links={[
-          {
-            label: "Link 1",
-            href: "",
-          },
-          {
-            label: "Link 2",
-            href: "",
-          },
-          {
-            label: "Link 3",
-            href: "",
-          },
-        ]}
-      ></Breadcrumb>
-    );
+    mount(<Breadcrumb links={links}></Breadcrumb>);
+  });
+
+  it("should render a nav element", function () {
+    const wrapper = mount(<Breadcrumb links={links}></Breadcrumb>);
+    expect(wrapper.find("nav")).toHaveLength(1);
   });
 });
