014ef0d3e9ca2e09f5cc6601c65b75d4fc1d53e2
Adam Birse <adambirse@gmail.com>
2022-02-02 14:16:28 +0000

    Breadcrumb added to page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { Breadcrumb } from "../components/Breadcrumb/Breadcrumb";
+
 export default function Page() {
   return (
     <>
       <h1>My NextJS bootstrap project</h1>
-      <p>A starter for 10 project to bootstrap nextJS development</p>
-    </>
+      <Breadcrumb/>
+      </>
   );
 }
