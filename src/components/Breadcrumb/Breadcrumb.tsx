16432e1fb629b0944226a0640fa78b58bddee4c4
Adam Birse <adambirse@gmail.com>
2022-02-02 14:22:32 +0000

    Add SASS support.

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
+
+import styles from './Breadcrumb.module.scss';
+
 
 export const Breadcrumb: React.FC = () => {
-    return <p >Hello</p>;
-  };
+  return (
+    <nav className={styles.BreadcrumbContainer}>
+      <ol>
+        <li>
+          <a href="">Link 1</a>
+        </li>
+        <li>
+          <a href="">Link 2</a>
+        </li>
+        <li>
+          <a href="">Link 3</a>
+        </li>
+      </ol>
+    </nav>
+  );
+};
